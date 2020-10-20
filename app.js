022d252330d20295c6d96b804d3c20078df31992
blessedliv <belovedpeaceliv@gmail.com>
2020-10-20 22:20:32 +0900

    redirect

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const path = require('path');   // const ==> reference type 
 const express = require('express');
 
-const {nowDateKorean, nowDateIso} = require('./modules/sample');
+const {nowDateKorean, nowDateIso} = require('./modules/date');
 
+const notFound = path.join(__dirname , 'public/404.html');
 
-console.log(nowDateKorean());
-console.log(nowDateIso());
+// console.log(nowDateKorean());
+// console.log(nowDateIso());
+
+const app = express();
+app.listen(3000, ()=>{console.log('http://127.0.0.1:3000')});
+
+app.use('/', express.static(path.join(__dirname,'public')));
+
+app.get('/', (req,res)=>{
+    res.send('<h1>Hello</h1>');
+});
+
+//404 error pages
+app.use((req, res)=> {
+ //   res.redirect('/404.html');
+ res.sendFile(notFound);
+});
